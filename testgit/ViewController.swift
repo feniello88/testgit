ad9ab06ff5519c2936c32d2a9ceda9c0170d4807
Armando Feniello <armando@10-20-49-91.0200.fed.it.iosda.org>
2017-12-07 17:09:33 +0100

    Initial Commit

---
+++ testgit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testgit
+//
+//  Created by Armando Feniello on 07/12/17.
+//  Copyright © 2017 Armando Feniello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
